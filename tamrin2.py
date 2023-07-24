938135e73f120bd490bd1245314963629009061b
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-07-24 23:59:04 +0330

    Add files via upload

---
+++ tamrin2.py
@@ -0,0 +1,6 @@
+#start
+num =input("Enter a number: ")
+num = float(num)
+num_plus_2 = num + 2
+print(num_plus_2)
+#end
---
+++ tamrin3.py
@@ -0,0 +1,9 @@
+#start
+doller = int(input("how many dollars do you have? "))
+print(doller)
+europ = int(input("how many europ do you have? "))
+print(europ)
+rate = int(input("how many rate do you have? "))
+print(rate)
+europ = doller * rate 
+print(europ)
