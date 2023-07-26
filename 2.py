573a55136b8e73a3a8f5723c0e012e167f592046
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-07-26 16:14:04 +0330

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+a = int(input( 'Enter the first number: '))
+print(a)
+f = int(input('Enter the second number: '))
+print(f)
+sum = a + f
+print(sum)
+subtractiong = a - f
+print(subtractiong)
+Question = input('What should I do for you? ')
+if Question == sum:
+    print(sum)
+if Question == subtractiong:
+    print(subtractiong)
+
+
