82bbe8f3da5931795d8b089d972e8b2eefcbbb6c
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-01 09:54:17 +0330

    Add files via upload

---
+++ assignment 01/number math.py
@@ -0,0 +1,6 @@
+ashar = float(input('ashar: '))
+yekan = int(input('yekan: '))
+dahgan = int(input('dahgan: '))
+sadgan = int(input('sadgan: '))
+hezargan = int(input('hezargan: '))
+print(ashar * 0.1,yekan * 1,dahgan * 10,sadgan * 100,hezargan * 1000)
