d8d111e03ddea5c24ca6ea491f512f23da7cccd3
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-03 01:37:20 +0330

    Add files via upload

---
+++ assignment 01/site.py
@@ -0,0 +1,7 @@
+username = input('please enter your username: ')
+password = input('please enter your password: ')
+if username == username:
+    if password == password:
+        print("your username and password is true.!")
+else:
+    print("your username and password is flase!!!! please check again.")
