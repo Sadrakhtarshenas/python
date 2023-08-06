bf5234e890f24e8f07968eb13b559043ff4be7ea
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-06 23:55:42 +0330

    Add files via upload

---
+++ assignment03/metode.py
@@ -0,0 +1,15 @@
+# a = "class python"
+# a = a.center(60, "O")
+# print(a)
+
+
+
+
+# name = "my name is sadra"
+# name.encode()
+
+# txt = "My name is Ståle"
+# txt.encode()
+
+d = "s/a/d/r/a/"
+d = d.expandtabs(2)
