936990c4a152641192530c295f7e9c2dbd25e1c7
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-07 19:59:37 +0330

    Add files via upload

---
+++ assignment03/name.py
@@ -0,0 +1,6 @@
+name_family = str(input("Enter your name: "))
+if name_family == int or float:
+    print("your name is uncorect!!!")
+name_family = name_family.capitalize()
+print( "your name and family is:", name_family)
+
