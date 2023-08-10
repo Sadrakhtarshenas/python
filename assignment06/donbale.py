6c850b82c15f808c79c0609bda927941ba833ba0
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-10 14:45:28 +0330

    Add files via upload

---
+++ assignment06/donbale.py
@@ -0,0 +1,6 @@
+
+a = int(input("number: "))
+b = 1
+while b <= a:
+    print(b,"'" * (b * 2), sep = '', end = '')
+    b += 1
---
+++ assignment06/number.py
@@ -0,0 +1,8 @@
+num = float(input("Enter your number: "))
+num2 = float(input("enter your number: "))
+num3 = float(input("enter your number: "))
+num4 = float(input("enter your number: "))
+mylist = [num,num2,num3,num4]
+mylist.reverse()
+mylist.sort()
+print(mylist)
