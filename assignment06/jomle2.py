fe7985504cce453c8f4c76a54c4964c9bd770273
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-10 14:45:53 +0330

    Add files via upload

---
+++ assignment06/jomle2.py
@@ -0,0 +1,8 @@
+name1 = str(input("name1: "))
+name2 = str(input("name2: "))
+name3 = str(input("name3: "))
+name4 = str(input("name4: "))
+
+list1 = ['ashkan','milad','daryosh','mamad','ahmad','saied']
+list2 = [name1,name2,name3,name4]
+print(list1,list2)
