bf987fb7fe6ac7a2d80abe1ec3992c80c8e2d34a
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-13 17:03:18 +0330

    Add files via upload

---
+++ assignment07/atm.py
@@ -0,0 +1,16 @@
+f = 0
+while f < 3:
+    passw = input("enter your password: ")
+    passw1 = str(passw)
+    if len (passw1) == 4 and int:
+        pass_asly = 1386
+        if passw == 1386:
+            print("your password is crect.")
+            print("hi welcome to ATM.")
+            break
+
+        elif passw == 8965 or float or str:
+            print("call the police")
+            break
+
+
