495ddef7a2c3c68e57f95a610623d17dfc04b642
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-15 01:20:53 +0330

    Add files via upload

---
+++ assignment09/list8.py
@@ -0,0 +1,10 @@
+list0 = ["py","i"," ","popul","lang"]
+list1 = ["thon","s","a","ar","uage."]
+new_list = []
+new_list.append(list0[0] + list1[0])
+new_list.append(list0[1] + list1[1])
+new_list.append(list0[2] + list1[2])
+new_list.append(list0[3] + list1[3])
+new_list.append(list0[4] + list1[4])
+print(new_list,'🐍🐍💪')
+
