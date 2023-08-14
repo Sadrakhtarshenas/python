cec7ebb272b99f575ccc74d07d861ebce7feb562
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-15 00:59:09 +0330

    Add files via upload

---
+++ assignment09/palindrome 8.py
@@ -0,0 +1,9 @@
+inp = input('enter you number or str: ')
+vlu = inp
+inp = list(inp)
+vlu = list(vlu)
+vlu.reverse()
+if vlu == inp :
+    print('yes that corect..')
+else:
+    print('no is false:///')
