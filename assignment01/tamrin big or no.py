fee464554700e5bf59216dad395dc625c1de2e10
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-17 11:38:27 +0330

    Delete tamrin big or no.py

--- assignment01/tamrin big or no.py
+++
@@ -1,8 +0,0 @@
-a = float(input("enter number: "))
-b = float(input("enter number: "))
-if a > b:
-       print("a is biger")
-elif b > a:
-    print("b is biger")
-else:
-    print('None is greater')
