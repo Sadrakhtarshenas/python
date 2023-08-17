df0a9e25baff57267e0de61695f2503b0bf82b48
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-17 12:31:27 +0330

    Add files via upload

---
+++ assignment07/list.py
@@ -0,0 +1,9 @@
+list1 = ["py","i"," ","popul","lang"]
+new_list = ["thon","s","a","ar","uage."]
+new_list2 = []
+new_list2.append(list1[0] + new_list[0])
+new_list2.append(list1[1] + new_list[1])
+new_list2.append(list1[2] + new_list[2])
+new_list2.append(list1[3] + new_list[3])
+new_list2.append(list1[4] + new_list[4])
+print(new_list2)
