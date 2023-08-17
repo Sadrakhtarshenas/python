75b74cb606fe6be6baea849abca8265b3613db8e
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-17 12:18:58 +0330

    Delete number.py

--- assignment06/number.py
+++
@@ -1,8 +0,0 @@
-num = float(input("Enter your number: "))
-num2 = float(input("enter your number: "))
-num3 = float(input("enter your number: "))
-num4 = float(input("enter your number: "))
-mylist = [num,num2,num3,num4]
-mylist.reverse()
-mylist.sort()
-print(mylist)
