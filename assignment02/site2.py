94dfb84e7a1a00b36c127344723a4bae1cbf1767
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-17 11:41:40 +0330

    Delete assignment02 directory

--- assignment02/site2.py
+++
@@ -1,8 +0,0 @@
-username = str(input("enter your usernam: "))
-password = input("enter your password: ")
-if username == int or float:
-    print("your username is false!!")
-if password == str:
-    print("your password is false!!!")
-else:
-    print("your username and password is", usename , password )
