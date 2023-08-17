b9cba3b5e3fda8ba9d802578922915193c43cae2
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-17 11:46:08 +0330

    Delete name.py

--- assignment03/name.py
+++
@@ -1,6 +0,0 @@
-name_family = str(input("Enter your name: "))
-if name_family == int or float:
-    print("your name is uncorect!!!")
-name_family = name_family.capitalize()
-print( "your name and family is:", name_family)
-
