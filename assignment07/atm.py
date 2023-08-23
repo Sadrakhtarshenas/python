6ea9adb6b2ce7c3b7b223cd3bde135cb7d921b16
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-23 16:58:10 +0330

    Add files via upload

---
+++ assignment07/atm.py
@@ -0,0 +1,27 @@
+password = "1386"
+false_password = 1
+conter = 0
+while password == "1386":
+    password_input = input("enter youre password:")
+    if password_input == password:
+        print("log in")
+        exit()
+    if str(len(password_input)) < str(len(password)):
+        print("The number of characters in the password is low")
+        password_input = "0000"
+    list_password = list(password)
+    list_password_input = list(password_input)
+    for i in range(4):
+        if list_password[i] == list_password_input[(i+1) * -1]:
+            conter += 1
+    if false_password == 3:
+        print("lock!")
+        exit()
+    elif conter == 4:
+        print("call to police")
+        exit()
+    elif str(len(password_input)) > str(len(password)):
+        password == "1386"
+    else:
+        print("The password is wrong!")
+        false_password += 1
