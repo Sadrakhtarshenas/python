1fc5710b3344ddb2ca8774f50df07da3febc3394
Sadrakhtarshenas <140339193+Sadrakhtarshenas@users.noreply.github.com>
2023-08-24 15:14:50 +0330

    Add files via upload

---
+++ assignment13/person.py
@@ -0,0 +1,12 @@
+class Person:
+    Name = None
+    Family = None
+    Age = None
+    id_card  = None
+
+
+    def characteristics():
+        pass
+    
+    def Individual_profile ():
+        pass
